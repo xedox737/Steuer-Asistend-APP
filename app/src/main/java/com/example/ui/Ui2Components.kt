031123a31304c7fd837c0a16efcfabefefe79f5d
xedox737 <sergej-gerweck@web.de>
2026-09-15 00:07:35 +0200

    Add Compose sp unit import

--- app/src/main/java/com/example/ui/Ui2Components.kt
+++ app/src/main/java/com/example/ui/Ui2Components.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /** Shared presentation tokens. These components do not own application state. */
 object Ui2 {
