d2f1eb8bd388da8f5b0a3a303081f0defdb3f66e
xedox737 <sergej-gerweck@web.de>
2026-09-14 02:36:23 +0200

    UI 2.0: introduce shared presentation tokens and responsive cards

--- app/src/main/java/com/example/ui/theme/Color.kt
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -6,6 +6,7 @@ val Purple80 = Color(0xFFBFDBFE)
 val PurpleGrey80 = Color(0xFFCBD5E1)
 val Pink80 = Color(0xFF99F6E4)
 
-val Purple40 = Color(0xFF2563EB)
+val Purple40 = Color(0xFF0066FF)
 val PurpleGrey40 = Color(0xFF475569)
 val Pink40 = Color(0xFF059669)
+
