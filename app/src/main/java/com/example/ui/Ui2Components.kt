87b649d91ef870910bc4021917ba73dca6b10690
xedox737 <sergej-gerweck@web.de>
2026-09-15 10:01:33 +0200

    Kompakte Action-Parameter typisieren

--- app/src/main/java/com/example/ui/Ui2Components.kt
+++ app/src/main/java/com/example/ui/Ui2Components.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
